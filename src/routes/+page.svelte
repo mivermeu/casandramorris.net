35b69b7850aa441426ac7972358e6b8169e14179
Milo Vermeulen <milovermeulen@me.com>
2025-07-09 21:58:22 -0400

    Add placeholder front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Casandra's Portfolio</title>
+	<style>
+		body {
+			background-color: #0f0;
+			font-family:
+				Comic Sans MS,
+				sans-serif;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			height: 100vh;
+			margin: 0;
+		}
+		.container {
+			text-align: center;
+		}
+		h1 {
+			color: #f00;
+		}
+		p {
+			color: #00f;
+		}
+	</style>
+</svelte:head>
+
+<div class="container">
+	<h1>Welcome to my website!</h1>
+	<p>Wow, would you look at that. I have a website.</p>
+</div>
